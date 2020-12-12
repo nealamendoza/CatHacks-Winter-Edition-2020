ac795951a98bcc5d42ff84169fe5d484e4a46a24
anishdhandore <67917438+anishdhandore@users.noreply.github.com>
2020-12-12 07:39:49 +0530

    algorithm for checking profanity

--- webscraper.py
+++ webscraper.py
@@ -1,1 +1,21 @@
-import bs4
+import bs4, requests, lxml
+import json
+import re
+from pprint import pprint
+from better_profanity import profanity
+
+def profanity_check(url):
+    # data from the website we will scrape 
+    info = requests.get(url)
+    soup = bs4.BeautifulSoup(info.text, "lxml")
+    my_data = ""
+    my_data += soup.text
+    #pprint(my_data)
+    if profanity.contains_profanity(my_data):
+        profane_text = profanity.censor(my_data)
+        return f"Not for kids {profane_text}"
+    else:
+        return "kids, here you go"
+   
+print(profanity_check("https://www.commonsensemedia.org/lists/kid-safe-browsers-and-search-sites"))
+#data("https://www.dictionary.com/browse/fuck")
