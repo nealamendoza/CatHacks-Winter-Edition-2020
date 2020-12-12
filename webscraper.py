f85a06039dd987cb1104b80115cdf9b81e753bf4
anishdhandore <67917438+anishdhandore@users.noreply.github.com>
2020-12-12 13:17:15 +0530

    home page outline

--- webscraper.py
+++ webscraper.py
@@ -10,32 +10,18 @@ def profanity_check(url):
     soup = bs4.BeautifulSoup(info.text, "lxml")
     my_data = ""
     my_data += soup.text
-<<<<<<< HEAD
     data = my_data.split()
     #print(data[0])
-=======
-
-
-    word = ""
-    list_of_words = []
-    for i in range(len(soup.text)-1):
-        #print(soup.text[i])
-        if((soup.text[i] == ' ') and (len(word) > 0)):
-            list_of_words.append(word)
-            word = ""
-        else:
-            if(soup.text[i] + soup.text[i+1] != "\n" ):
-                word += soup.text[i]
-
-
-    print(list_of_words)
->>>>>>> fc81e14cd79256672ec45ce698c46dd063efe144
 
     #pprint(my_data)
-    if profanity.contains_profanity(data):
+    words = 0
+    for i in range(len(data)):
+        #words = 0
+        if profanity.contains_profanity(data[i]):
         #profane_text = profanity.censor(my_data)
-        return "Not for kids"
-    else:
-        return "kids, here you go"
+            print(data[i])
+            words += 1
+    return words
+    
 
-print(profanity_check(Enter URL))
+print(profanity_check("https://www.dictionary.com/browse/fuck"))
