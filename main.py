5233ac152517fbecaee495bf85c215dba9e4f806
kylejava <kyleedwardgjava@gmail.com>
2020-12-11 17:47:06 -0800

    put files in stylesheets folder

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World")
+import requests
+from flask import Flask, render_template, flash, request
+from functions import *
+from webscraper import *
+app = Flask(__name__)
+
+@app.route('/' , methods = ['GET' , 'POST'])
+def home():
+    return render_template('index.html')
+
+@app.route('/scan' , methods = ['GET' , 'POST'])
+def scan():
+    return render_template('verify.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
