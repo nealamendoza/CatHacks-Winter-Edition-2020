8e93f57c63106006b14bf28780c5ea2d34b9c7dd
anishdhandore <67917438+anishdhandore@users.noreply.github.com>
2020-12-12 21:00:06 +0530

    faster&better algorithm

--- webscraper.py
+++ webscraper.py
@@ -1,27 +1,48 @@
-import bs4, requests, lxml
+import bs4
+import requests
+import lxml
 import json
 import re
 from pprint import pprint
-from better_profanity import profanity
+#from better_profanity import profanity
 
-def profanity_check(url):
-    # data from the website we will scrape
-    info = requests.get(url)
+#dirty_text = "How is it going bitch"
+#if profanity.contains_profanity(dirty_text):
+    #print("Contains profanity")
+#else:
+    #print("This website is safe")
+
+def data(url):
+    # list of abusive words
+    info = requests.get("https://www.cs.cmu.edu/~biglou/resources/bad-words.txt")
     soup = bs4.BeautifulSoup(info.text, "lxml")
-    my_data = ""
-    my_data += soup.text
-    data = my_data.split()
-    #print(data[0])
+    abusive_words = []
+    abusive_words = soup.text.split()
+    #print(abusive_words)
 
-    #pprint(my_data)
-    words = 0
-    for i in range(len(data)):
-        #words = 0
-        if profanity.contains_profanity(data[i]):
-        #profane_text = profanity.censor(my_data)
-            print(data[i])
-            words += 1
-    return words
+    # data from the website we will scrape 
+    info_url = requests.get(url)
+    soup_url = bs4.BeautifulSoup(info_url.text, "lxml")
+    my_data = []
+    my_data = soup_url.text.split()
+    #print(my_data)
+
+    count = 0
+    mal_words = []
+    for word in range(len(my_data)):
+        for abusive_word in range(len(abusive_words)):
+            if my_data[word] == abusive_words[abusive_word]:
+                mal_words.append(abusive_words[abusive_word])
+                count += 1
+    if count == 0:
+        return "this website is safe"
+    else:
+        return f"contains {count} malicious words"
+
+    #return [count, mal_words]
+
+
+
     
-
-print(profanity_check("https://www.dictionary.com/browse/fuck"))
+print(data("https://www.dictionary.com/browse/beauty"))
+#data("https://www.dictionary.com/browse/fuck")
